aa6ff5ca6a686f4fe64ef3ff3f7fed5ffc7570de
falgon <falgon53@gmail.com>
2018-12-03 23:37:07 +0900

    update

--- src/Math/Matrix/LU.hs
+++ src/Math/Matrix/LU.hs
@@ -3,11 +3,14 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module Math.Matrix.LU (
-    doolittleST
+    PLU (..),
+    fromPLU,
+    doolittleST,
+    doolittleST'
 ) where
 
-import Data.Array.Unboxed (UArray)
-import Data.Array.ST (Ix, MArray, STUArray, newListArray, readArray, writeArray, freeze)
+import Data.Array.IArray (IArray, bounds, listArray, (!))
+import Data.Array.ST (Ix, MArray, STUArray, newListArray, readArray, writeArray, thaw, freeze)
 import Data.Bool (bool)
 import Data.Foldable (foldrM)
 import Data.Tuple.Extra (first, second, dupe)
@@ -15,10 +18,17 @@ import Control.Monad (forM_, when)
 import Control.Monad.Fix (fix)
 import Control.Monad.ST (ST, runST)
 
+{-
 {-# INLINE from2dList #-}
 from2dList :: MArray a e m => [[e]] -> m (a (Int, Int) e)
 from2dList [] = newListArray ((0, 0), (0, 0)) []
 from2dList xs = newListArray ((0, 0), (pred $ length xs, pred $ length $ head xs)) $ concat xs
+-}
+
+{-# INLINE to2dArray #-}
+to2dArray :: IArray a e => [[e]] -> a (Int, Int) e
+to2dArray [] = listArray ((0, 0), (0, 0)) []
+to2dArray xs = listArray ((0, 0), (pred $ length xs, pred $ length $ head xs)) $ concat xs
 
 {-# INLINE swapMArray #-}
 swapMArray :: (MArray a e m, Ix i) => a i e -> i -> i -> m ()
@@ -28,37 +38,7 @@ swapMArray ar fi ti = do
     writeArray ar fi to
 
 {-
-forwardST :: [[Double]] -> Maybe ([Int], [[Double]]) 
-forwardST m = let len = length $ head m in
-    Just $ first fst $ second (unfoldr (\x -> if length x /= 0 then Just (take len x, drop len x) else Nothing) . snd) $ dupe $ runST $ do
-        ar <- from2dList m :: ST s (STUArray s (Int, Int) Double)
-        pr <- newListArray (0, pred len) [0..pred len] :: ST s (STUArray s Int Int)
-        forM_ [0..pred len] $ \i -> do
-            now <- readArray ar (i, i)
-            r <- flip (flip foldrM i) [succ i..pred len] $ \k acc -> do
-                next <- readArray ar (k, i)
-                return $ if abs next > abs now then k else acc
-            --if readArray ar (r, i) == 0
-            when (r /= i) $ do
-                swapMArray pr r i
-                forM_ [i..pred len] $ \k -> swapMArray ar (i, k) (r, k)
-            
-            forM_ [succ i..pred len] $ \k -> do
-                ki <- readArray ar (k, i)
-                ii <- readArray ar (i, i)
-                writeArray ar (k, i) (ki / ii)
-                forM_ [succ i, pred len] $ \j -> do
-                    kj <- readArray ar (k, j)
-                    ij <- readArray ar (i, j)
-                    kii <- readArray ar (k, i)
-                    writeArray ar (k, j) (kj - ij * kii)
-        a <- getElems ar
-        p <- getElems pr
-        return (p, a)
--}
-   -- maybe Nothing (Just . first fst . second (unfoldr (\x -> if length x /= 0 then Just (take len x, drop len x) else Nothing) . snd) . dupe) $ runST $ do
-
-forwardST :: [[Double]] -> Maybe (UArray Int Int, UArray (Int, Int) Double)
+forwardST :: (IArray a Int, IArray a Double) => [[Double]] -> Maybe (a Int Int, a (Int, Int) Double) 
 forwardST m = let len = length $ head m in runST $ do
         ar <- from2dList m :: ST s (STUArray s (Int, Int) Double)
         pr <- newListArray (0, pred len) [0..pred len] :: ST s (STUArray s Int Int)
@@ -78,14 +58,56 @@ forwardST m = let len = length $ head m in runST $ do
                         kii <- readArray ar (k, i)
                         writeArray ar (k, j) (kj - ij * kii)
                 f $ succ i
+-}
 
--- doolittleST :: [[Double]] -> Maybe ([Int], [[Double]])
--- doolittleST :: [[Double]] -> Maybe (IArray s (Int, Double)
-doolittleST :: [[Double]] -> Maybe (UArray Int Int, UArray (Int, Int) Double)
+data PLU a i e = PLU (a (i, i) e)
+
+instance (Ix i, Enum i, IArray a e, Show e) => Show (PLU a i e) where
+    show (PLU ar) = "\n" ++ concat (flip map [fst $ fst $ bounds ar..fst $ snd $ bounds ar] $ \i ->
+        "{\t" ++ (concat $ flip map [snd $ fst $ bounds ar..snd $ snd $ bounds ar] $ \j -> show (ar ! (i, j)) ++ "\t") ++ "}\n")
+
+fromPLU :: PLU a i e -> a (i, i) e
+fromPLU (PLU ar) = ar
+
+forwardST :: (IArray a Int, IArray a Double) => a (Int, Int) Double -> Maybe (a Int Int, PLU a Int Double) 
+forwardST m = let len = succ $ snd $ snd $ bounds m in runST $ do
+        ar <- thaw m :: ST s (STUArray s (Int, Int) Double)
+        pr <- newListArray (0, pred len) [0..pred len] :: ST s (STUArray s Int Int)
+        ($ 0) . fix $ \f i -> if i >= len then ((.) Just . (.) (second PLU) . (,))  <$> freeze pr <*> freeze ar else do
+            now <- readArray ar (i, i)
+            r <- foldrM (\k acc -> bool acc k . (flip (>) (abs now)) . abs <$> readArray ar (k, i)) i [succ i..pred len]
+            maxn <- readArray ar (r, i)
+            if maxn == 0 then return Nothing else do
+                when (r /= i) $ do
+                    swapMArray pr r i
+                    forM_ [i..pred len] $ uncurry (swapMArray ar) . first (i,) . second (r,) . dupe 
+                forM_ [succ i..pred len] $ \k -> do
+                    writeArray ar (k, i) =<< (/) <$> readArray ar (k, i) <*> readArray ar (i, i)
+                    forM_ [succ i..pred len] $ \j -> do
+                        kj <- readArray ar (k, j)
+                        ij <- readArray ar (i, j)
+                        kii <- readArray ar (k, i)
+                        writeArray ar (k, j) (kj - ij * kii)
+                f $ succ i
+
+doolittleST :: (IArray a Int, IArray a Double) => a (Int, Int) Double -> Maybe (a Int Int, PLU a Int Double)
+doolittleST m 
+    | (==0) . fst $ snd $ bounds m = Nothing
+    | uncurry (==) $ snd $ bounds m = forwardST m
+    | otherwise = Nothing
+
+
+doolittleST' :: (IArray a Int, IArray a Double) => [[Double]] -> Maybe (a Int Int, PLU a Int Double)
+doolittleST' = doolittleST . to2dArray 
+
+-- | doolittleST
+-- If it is a square and regular matrix, 
+-- it returns the exchange information of the row permutation and the LU-decomposed array(It is grouped in one matrix).
+-- Otherwise it returns Nothing.
+{-
+doolittleST :: (IArray a Int, IArray a Double) => [[Double]] -> Maybe (a Int Int, a (Int, Int) Double) 
 doolittleST [] = Nothing
 doolittleST m 
     | (length m > 1) && all ((>= length m) . length) m = forwardST m
     | otherwise = Nothing
-
-
-
+-}
