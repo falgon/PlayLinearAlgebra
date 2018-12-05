e62b143c76c8e3c07a38a72872515bec8b71e9f7
falgon <falgon53@gmail.com>
2018-12-05 19:07:29 +0900

    complete LU factorization

--- src/Math/Matrix/LU.hs
+++ src/Math/Matrix/LU.hs
@@ -3,10 +3,9 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module Math.Matrix.LU (
- --   setMaxAbsAt,
-    forward,
     PLU (..),
     fromPLU,
+    doolittle,
     doolittleST,
     doolittleST'
 ) where
@@ -63,16 +62,17 @@ forwardST m = let len = length $ head m in runST $ do
                 f $ succ i
 -}
 
-data PLU a i e = PLU (a (i, i) e)
+newtype PLU a i e = PLU (a (i, i) e)
 
 instance (Ix i, Enum i, IArray a e, Show e) => Show (PLU a i e) where
     show (PLU ar) = "\n" ++ concat (flip map [fst $ fst $ bounds ar..fst $ snd $ bounds ar] $ \i ->
-        "{\t" ++ (concat $ flip map [snd $ fst $ bounds ar..snd $ snd $ bounds ar] $ \j -> show (ar ! (i, j)) ++ "\t") ++ "}\n")
+        "{\t" ++ concat (flip map [snd $ fst $ bounds ar..snd $ snd $ bounds ar] $ \j -> show (ar ! (i, j)) ++ "\t") ++ "}\n")
 
 {-# INLINE fromPLU #-}
 fromPLU :: PLU a i e -> a (i, i) e
 fromPLU (PLU ar) = ar
 
+{-# INLINE swapAt #-}
 swapAt :: Int -> Int -> [a] -> [a]
 swapAt ii jj mx
     | ii == jj = mx
@@ -82,27 +82,32 @@ swapAt ii jj mx
 setMaxAbsAt :: (Num a, Ord a) => Int -> [Int] -> [[a]] -> ([Int], [[a]])
 setMaxAbsAt i p m
     | length m <= i || length m < 2 || length m /= length p = (p, m)
-    | otherwise = let rep = foldl1' (\x acc -> if abs ((m !! x) !! i) > abs ((m !! acc) !! i) then x else acc) $ [i..pred $ length m] in (swapAt i rep p, swapAt i rep m)
+    | otherwise = let rep = foldl1' (\x acc -> if abs ((m !! x) !! i) > abs ((m !! acc) !! i) then x else acc) [i..pred $ length m] in (swapAt i rep p, swapAt i rep m)
 
--- forward :: Real a => [[a]] -> Maybe ([Int], [[a]])
+forward :: (Fractional e, IArray a1 Int, IArray a2 e, Ord e) => [[e]] -> Maybe (a1 Int Int, PLU a2 Int e)
 forward m
     | length m < 2 || any ((< 2) . length) m = Nothing
     | all ((==) (length m) . length) m = Just $ forward' 0 [0..pred $ length m] [] m
     | otherwise = Nothing
         where
-            elim :: (Fractional a, Real a) => Int -> [[a]] -> ([a], [[a]])
             elim i mx = let use = (mx !! i) !! i; l = map ((/use) . flip (!!) i) $ drop (succ i) mx in 
                 (l, take (succ i) mx ++ zipWith (\row ll -> zipWith (+) row $ map (negate . (*ll)) (mx !! i)) (drop (succ i) mx) l)
             
-            toUnite :: Int -> [[a]] -> [[a]] -> [[a]]
             toUnite _ [] u = u
             toUnite i (l:ls) u = let (uu:uus) = zipWith (\al urow -> take i urow ++ [al] ++ drop (succ i) urow) l u in uu : toUnite (succ i) ls uus
 
-            forward' :: (Fractional a, Real a) => Int -> [Int] -> [[a]] -> [[a]] -> ([Int], [[a]])
             forward' i p l u
                 | i < pred (length u) = let (pp, mx) = setMaxAbsAt i p u in uncurry (forward' (succ i) pp) $ first ((l ++) . (:[])) $ elim i mx
-                | otherwise = (p, head u : toUnite 0 l (tail u))
+                | otherwise = (listArray (0, pred $ length p) p, PLU $ to2dArray (head u : toUnite 0 l (tail u)))
 
+-- | doolittleST
+-- If it is a square and regular matrix, 
+-- it returns the exchange information of the row permutation and the LU-decomposed array(It is grouped in one matrix).
+-- Otherwise it returns Nothing. Use List for LU factorization.
+doolittle ::(Fractional e, IArray a1 Int, IArray a2 e, Ord e) => [[e]] -> Maybe (a1 Int Int, PLU a2 Int e)
+doolittle m
+    | length m > 1 && all ((==(length m)) . length) m = forward m
+    | otherwise = Nothing
 
 forwardST :: (IArray a Int, IArray a Double) => a (Int, Int) Double -> Maybe (a Int Int, PLU a Int Double) 
 forwardST m = let len = succ $ snd $ snd $ bounds m in runST $ do
@@ -110,7 +115,7 @@ forwardST m = let len = succ $ snd $ snd $ bounds m in runST $ do
         pr <- newListArray (0, pred len) [0..pred len] :: ST s (STUArray s Int Int)
         ($ 0) . fix $ \f i -> if i >= len then ((.) Just . (.) (second PLU) . (,))  <$> freeze pr <*> freeze ar else do
             now <- readArray ar (i, i)
-            r <- foldrM (\k acc -> bool acc k . (flip (>) (abs now)) . abs <$> readArray ar (k, i)) i [succ i..pred len]
+            r <- foldrM (\k acc -> bool acc k . (> abs now) . abs <$> readArray ar (k, i)) i [succ i..pred len]
             maxn <- readArray ar (r, i)
             if maxn == 0 then return Nothing else do
                 when (r /= i) $ do
@@ -125,7 +130,10 @@ forwardST m = let len = succ $ snd $ snd $ bounds m in runST $ do
                         writeArray ar (k, j) (kj - ij * kii)
                 f $ succ i
 
-
+-- | doolittleST
+-- If it is a square and regular matrix, 
+-- it returns the exchange information of the row permutation and the LU-decomposed array(It is grouped in one matrix).
+-- Otherwise it returns Nothing. Use ST Monad for LU factorization.
 doolittleST :: (IArray a Int, IArray a Double) => a (Int, Int) Double -> Maybe (a Int Int, PLU a Int Double)
 doolittleST m 
     | (==0) . fst $ snd $ bounds m = Nothing
@@ -133,17 +141,8 @@ doolittleST m
     | otherwise = Nothing
 
 
+-- | doolittleST'
+-- It is similar to doolittleST. Receive list as input.
 doolittleST' :: (IArray a Int, IArray a Double) => [[Double]] -> Maybe (a Int Int, PLU a Int Double)
 doolittleST' = doolittleST . to2dArray 
 
--- | doolittleST
--- If it is a square and regular matrix, 
--- it returns the exchange information of the row permutation and the LU-decomposed array(It is grouped in one matrix).
--- Otherwise it returns Nothing.
-{-
-doolittleST :: (IArray a Int, IArray a Double) => [[Double]] -> Maybe (a Int Int, a (Int, Int) Double) 
-doolittleST [] = Nothing
-doolittleST m 
-    | (length m > 1) && all ((>= length m) . length) m = forwardST m
-    | otherwise = Nothing
--}
